0da0766012a1df2206474dae11cd24cc6b9b3b3e
Clément Barbet <cbarbet@mbp-de-clement-1.home>
2023-11-20 16:04:35 +0100

    Fixed responsive on /norwegian

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Clément Barbet Website';
+export const SITE_DESCRIPTION = 'Clément Barbet Website';
